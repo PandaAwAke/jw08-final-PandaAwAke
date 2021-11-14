2cee65ff5b3bbe88b83a3f4678b277670345c059
Ma Yingshuo <191220080@smail.nju.edu.cn>
2021-11-14 12:08:36 +0800

    Project created

---
+++ GourdGame/src/main/java/com/pandaawake/gourdgame/App.java
@@ -0,0 +1,13 @@
+package com.pandaawake.gourdgame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
