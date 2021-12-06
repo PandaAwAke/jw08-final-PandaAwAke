3a892b3e9d86ac9256296eef1e89212b8af6eb20
Ma Yingshuo <191220080@smail.nju.edu.cn>
2021-12-06 20:45:01 +0800

    Better logging

--- GourdGame/src/main/java/com/mandas/tiled2d/core/Log.java
+++ GourdGame/src/main/java/com/mandas/tiled2d/core/Log.java
@@ -1,12 +1,12 @@
 package com.mandas.tiled2d.core;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log {
-    private static final Logger mandasLogger = LogManager.getLogger("Mandas Logger");
-    private static final Logger appLogger = LogManager.getLogger("App Logger");
+    private static final Logger mandasLogger = LogManager.getLogger("MandasLogger");
+    private static final Logger appLogger = LogManager.getLogger("AppLogger");
+    private static final Logger fileLogger = LogManager.getLogger("FileLogger");
 
     public static Logger mandas() {
         return mandasLogger;
@@ -14,6 +14,10 @@ public class Log {
 
     public static Logger app() {
         return appLogger;
+    }
+
+    public static Logger file() {
+        return fileLogger;
     }
 
 }
